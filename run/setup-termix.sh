0c25865b4cc9d51cbf8b00d284fc421afc38befc
hustcer <hustcer@outlook.com>
2024-12-11 15:29:53 +0800

    chore: Try to remove jq for run/setup-termix.sh

--- run/setup-termix.sh
+++ run/setup-termix.sh
@@ -3,6 +3,7 @@
 # Created: 2024/12/11 09:39:56
 # Description: Setup termix-nu on macOS or Linux.
 
+LATEST_VERSION='0.100.0'
 DEST_DIR='/usr/local/bin/'
 
 ASSETS=(
@@ -12,7 +13,7 @@ ASSETS=(
   ["linux_aarch64"]="aarch64-unknown-linux-musl"
 )
 
-LATEST_META='https://terminus-new-trantor.oss-cn-hangzhou.aliyuncs.com/open-tools/nushell/latest.json'
+BASE_URL='https://terminus-new-trantor.oss-cn-hangzhou.aliyuncs.com/open-tools/nushell'
 
 function is_installed() {
   command -v "$1" &> /dev/null
@@ -35,40 +36,32 @@ function get_versions() {
   echo "${versions[@]}"
 }
 
-function get_latest_versions() {
-  declare -A versions
-  version=$(curl -s "$LATEST_META" | jq -r '.version' | sed 's/v//')
-  versions["nu"]=$version
-  echo "${versions[@]}"
-}
-
 function install_or_update() {
   local bin=$1
   local platform=$2
   echo "Installing or updating $bin ..."
-  local latest=$(curl -s "$LATEST_META")
-  local assetName=$(echo "$latest" | jq -r ".assets[] | select(.name | test(\"${ASSETS[$platform]}\")) | .name")
+  local assetName=$(wget -qO - "$BASE_URL/latest.json" | grep name | cut -d '"' -f 4 | grep "${ASSETS[$platform]}")
   local pkg="/tmp/$assetName"
-  curl -L -o "$pkg" "${LATEST_META/latest.json/$assetName}"
+  wget -O "$pkg" "$BASE_URL/$assetName"
   if is_installed sudo; then
     sudo tar xzf "$pkg" -C "$DEST_DIR"
   else
     tar xzf "$pkg" -C "$DEST_DIR"
   fi
   rm "$pkg"
-  echo "$bin is installed successfully with version $(echo "$latest" | jq -r '.version')"
+  echo "$bin is installed successfully with version $LATEST_VERSION"
 }
 
 function main() {
-  platform="$(uname -s)_$(uname -m)"
   current=$(get_versions)
-  latest=$(get_latest_versions)
+  latest=$LATEST_VERSION
+  platform="$(uname -s)_$(uname -m)"
 
   echo "Current Nu version: $current"
   echo "Latest  Nu version: $latest"
 
   for bin in nu; do
-    if is_lower_ver "${current[$bin]}" "${latest[$bin]}"; then
+    if is_lower_ver "${current[$bin]}" "$LATEST_VERSION"; then
       install_or_update "$bin" "$platform"
     else
       echo "$bin is updated ..."
